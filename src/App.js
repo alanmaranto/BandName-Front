50d18ed8f22566c3d765533dec089f046ad4a7fe
alan <alanarostegui@hotmail.com>
2020-11-20 23:59:05 -0600

    feature(socket): add socket connection to on/off

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 import AddBand from "./components/AddBand";
-import BandList from './components/BandList';
+import BandList from "./components/BandList";
+import "./App.css";
+
+const connectSocketServer = () => {
+  const socket = io.connect("http://localhost:8080", {
+    transports: ["websocket"],
+  });
+  return socket;
+};
 
 function App() {
+  const [online, setOnline] = useState(false);
+  const [socket] = useState(connectSocketServer());
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true)
+    })
+  }, [ socket])
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false)
+    })
+  }, [ socket])
+
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service status:
-          <span className="text-success">Online</span>
-          <span className="text-danger">Offline</span>
+          {online ? (
+            <span className="text-success">Online</span>
+          ) : (
+            <span className="text-danger">Offline</span>
+          )}
         </p>
       </div>
 
