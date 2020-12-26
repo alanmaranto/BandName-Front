f334edbf2c3870ad4c4251c146158bdf383ec2dc
alan <alanarostegui@hotmail.com>
2020-12-25 20:22:02 -0600

    refactor: move config chart to a file

--- src/components/BandChart.js
+++ src/components/BandChart.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useContext } from "react";
 import { SocketContext } from "../context/SocketContext";
+import { graphConfig } from "./graphConfig";
 import { Chart } from "chart.js";
 
 const BandChart = () => {
   const { socket } = useContext(SocketContext);
+
+  const { backgroundColor, borderColor, options } = graphConfig;
 
   useEffect(() => {
     socket.on("current-bands", (bands) => {
@@ -21,36 +24,15 @@ const BandChart = () => {
           {
             label: "# of Votes",
             data: bands.map((band) => band.votes),
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
+            backgroundColor,
+            borderColor,
             borderWidth: 1,
           },
         ],
       },
-      options: {
-        animation: false,
-        scales: {
-          xAxes: [{ stacked: true }],
-        },
-      },
+      options,
     });
   };
-
-  useEffect(() => {}, []);
 
   return <canvas id="myChart"></canvas>;
 };
