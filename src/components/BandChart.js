38069cb8fd393cb41878c6516b229f51fbcae200
alan <alanarostegui@hotmail.com>
2021-03-27 16:57:32 -0600

    disable

--- src/components/BandChart.js
+++ src/components/BandChart.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useContext } from "react";
 import { SocketContext } from "../context/SocketContext";
 import { graphConfig } from "./graphConfig";
